829aeffd980ff43a45c753b7b664998af1e6f728
thi-truong <92915699+thi-truong@users.noreply.github.com>
2024-12-07 00:06:03 -0800

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,13 @@
-install.packages("ggplot2")
+install.packages("dplyr") #data functions
+install.packages("readr") #for reading csv files
+install.packages("DT") #to make nice data tables
+install.packages("knitr") #most useful for tables and knitr function
+install.packages("kableExtra") # used to make responsive tables
+install.packages("ggplot2") 
+install.packages("mapview") 
+install.packages("maps") # for city names
+install.packages("sf") # to convert data.frame to spatial objects
+install.packages("plotly")
+install.packages("leaflet") 
+install.packages("tigris") 
+install.packages("rayshader")
